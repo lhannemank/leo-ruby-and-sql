27860825aaad6a9049537c6744ed4e0aebd7f50e
Leo Hanneman <leo.hanneman@kellogg.northwestern.edu>
2024-01-30 17:30:04 +0000

    1129am

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -9,9 +9,27 @@ Contact.destroy_all
 # **************************
 
 # - Insert and read contact data for companies in the database
-
+apple = Company.find_by({"name" => "Apple"})
 # 1. insert new rows in the contacts table with relationship to a company
+new_contact = Contact.new
+new_contact["first_name"] = "Lar"
+new_contact["last_name"] = "Hanivar"
+new_contact["email"] = "lar@hanivar.gov"
+new_contact["company_id"] = apple["id"]
+new_contact.save
+
+new_contact = Contact.new
+new_contact["first_name"] = "Andrew"
+new_contact["last_name"] = "Stupak"
+new_contact["email"] = "as@openai.com"
+new_contact["company_id"] = apple["id"]
+new_contact.save
 
 # 2. How many contacts work at Apple?
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+puts "Apple contacts: #{apple_contacts.count}"
 
 # 3. What is the full name of each contact who works at Apple?
+for person in apple_contacts
+    puts "#{person["first_name"]} #{person["last_name"]}"
+end
